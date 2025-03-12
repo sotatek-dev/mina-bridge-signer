47e6e3ebfea9f0dd75fa1680cde259e3b487171c
Sotatek-TanHoang <tan.hoang@sotatek.com>
2025-03-12 14:20:16 +0700

    feat: update cache

--- src/contract/base.js
+++ src/contract/base.js
@@ -6,25 +6,29 @@ import { FungibleToken } from 'mina-fungible-token';
 config()
 export class MinaScBuilder {
     constructor() {
-
-        const network = Mina.Network({
+        const testnet = {
+            mina: 'https://api.minascan.io/node/devnet/v1/graphql',
+            archive: 'https://api.minascan.io/archive/devnet/v1/graphql/',
+            networkId: 'testnet'
+        }
+        const mainnet = {
             mina: 'https://api.minascan.io/node/mainnet/v1/graphql',
             archive: 'https://api.minascan.io/archive/mainnet/v1/graphql/',
             networkId: 'mainnet'
-        });
+        }
+        const networkType = process.env['MINA_NETWORK_TYPE']
+        const network = Mina.Network(networkType === 'mainnet' ? mainnet : testnet);
         Mina.setActiveInstance(network);
     }
     async compileBridgeContract() {
         console.log('compiling bridge');
         await Bridge.compile({
-            cache: fileSystem(fetchFiles('bridge'))
         })
         console.log('compiling bridge done');
     }
     async compileTokenContract() {
         console.log('compiling token');
         await FungibleToken.compile({
-            cache: fileSystem(fetchFiles('token'))
         })
         console.log('compiling token done');
     }
